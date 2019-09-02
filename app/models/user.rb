9d114e03261bedb32bd8f5c81bec65c69c6e0367
janvier mutuyimana <janvierjavim2@gmail.com>
2019-09-02 10:33:58 +0000

    make it

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :posts
   validates :password, presence: true, length: { minimum: 6 }
+  validates :First_Name,  presence: true, length: { maximum: 19 }
+  validates :Email, presence: true, length: { maximum: 20 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
  
 end
